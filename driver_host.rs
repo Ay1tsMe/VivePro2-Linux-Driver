8157b0cdb6f831078fb2c5a8a7e57dc74a5b7de1
Adam <adam.wyatt@iinet.net.au>
2025-06-12 15:32:16 +0800

    Vive Cosmos code from MonsieurLanza repo

---
+++ driver_host.rs
@@ -0,0 +1,224 @@
+use cppvtbl::{impl_vtables, HasVtable, VtableRef, WithVtables};
+use lens_client::start_lens_server;
+use lens_protocol::{LensClient, StubClient};
+use once_cell::sync::Lazy;
+use std::env::var_os;
+use std::ffi::{CStr, OsString};
+use std::os::raw::c_char;
+use std::process::Command;
+use std::rc::Rc;
+use tracing::{error, info, warn};
+use vive_hid::{SteamDevice, ViveCosmosDevice};
+
+use crate::driver_context::DRIVER_CONTEXT;
+use crate::hmd::HmdDriver;
+use crate::openvr::{
+	Compositor_FrameTiming, DriverPose_t, ETrackedDeviceClass, EVREventType, HmdMatrix34_t,
+	HmdRect2_t, ITrackedDeviceServerDriverVtable, IVRServerDriverHost_Version, TrackedDevicePose_t,
+	VREvent_t,
+};
+use crate::openvr::{IVRServerDriverHost, IVRServerDriverHostVtable, VREvent_Data_t};
+use crate::settings::Setting;
+use crate::{setting, Result};
+
+#[impl_vtables(IVRServerDriverHost)]
+pub struct DriverHost {
+	real: &'static VtableRef<IVRServerDriverHostVtable>,
+}
+
+const HMD_RESOLUTION: Setting<i32> = setting!("vivepro2", "resolution");
+const BRIGHTNESS: Setting<i32> = setting!("vivepro2", "brightness");
+const NOISE_CANCEL: Setting<bool> = setting!("vivepro2", "noiseCancel");
+
+impl IVRServerDriverHost for DriverHost {
+	fn TrackedDeviceAdded(
+		&self,
+		pchDeviceSerialNumber: *const c_char,
+		eDeviceClass: ETrackedDeviceClass,
+		pDriver: *const VtableRef<ITrackedDeviceServerDriverVtable>,
+	) -> bool {
+		let sn = unsafe { CStr::from_ptr(pchDeviceSerialNumber) }
+			.to_string_lossy()
+			.to_string();
+		info!("added tracked device: {sn:?} ({eDeviceClass:?})");
+		if eDeviceClass == ETrackedDeviceClass::TrackedDeviceClass_HMD {
+			let err: Result<()> = try {
+				// Steam part is opened for checking if this is really a needed HMD device
+				let _steam = Rc::new(SteamDevice::open(&sn)?);
+				// We don't know for sure this device serial
+				let vive = Rc::new(ViveCosmosDevice::open_first()?);
+
+				let mode = {
+					let res = HMD_RESOLUTION.get();
+					let modes = vive.query_modes();
+					let mode = *modes.iter().find(|m| m.id == res as u8).unwrap_or(
+						modes
+							.first()
+							.expect("device has at least one mode if opened"),
+					);
+					HMD_RESOLUTION.set(mode.id as i32);
+
+					//vive.set_mode(mode.id)?;
+					mode
+				};
+				//{
+				//	let nc = NOISE_CANCEL.get();
+				//	NOISE_CANCEL.set(nc);
+
+				//	vive.toggle_noise_canceling(nc)?;
+				//}
+				//{
+				//	let mut brightness = BRIGHTNESS.get();
+				//	if brightness == 0 {
+				//		brightness = 130;
+				//	}
+				//	BRIGHTNESS.set(brightness);
+
+				//	vive.set_brightness(brightness as u8)?;
+				//}
+
+				let vive_config = vive.read_config()?;
+
+				let lens = start_lens_server(vive_config.inhouse_lens_correction.clone())
+					.map(|v| Rc::new(v) as Rc<dyn LensClient>)
+					.unwrap_or_else(|e| {
+						let zenity = var_os("STEAM_ZENITY").unwrap_or_else(|| OsString::from("zenity"));
+						let mut cmd = Command::new(zenity);
+						cmd.arg("--no-wrap").arg("--error").arg("--text").arg(format!("Lens distortion helper is failed to launch, HMD image most probaly will be distorted and unusable.\nError: {e}\n\nMake sure you have any recent version of proton installed."));
+						match cmd.spawn().and_then(|p| p.wait_with_output()) {
+							Ok(v) => {
+								info!("zenity finished: {}\n{:?}\n{:?}", v.status, v.stdout, v.stderr)
+							},
+							Err(e) => {
+								warn!("fatal error remains unnoticed: {e}")
+							},
+						}
+						error!("lens server start failed: {e}");
+						Rc::new(StubClient)
+					});
+				let real = unsafe { VtableRef::from_raw(pDriver) };
+
+				let hmd = Box::leak(Box::new(WithVtables::new(HmdDriver {
+					// steam,
+					vive,
+					vive_config,
+					lens,
+					real,
+					mode,
+				})));
+
+				return self.real.TrackedDeviceAdded(
+					pchDeviceSerialNumber,
+					eDeviceClass,
+					HasVtable::<ITrackedDeviceServerDriverVtable>::get(hmd),
+				);
+			};
+			error!("failed to wrap hmd: {}", err.err().unwrap());
+		}
+		self.real
+			.TrackedDeviceAdded(pchDeviceSerialNumber, eDeviceClass, pDriver)
+	}
+
+	fn TrackedDevicePoseUpdated(
+		&self,
+		unWhichDevice: u32,
+		newPose: *const DriverPose_t,
+		unPoseStructSize: u32,
+	) {
+		self.real
+			.TrackedDevicePoseUpdated(unWhichDevice, newPose, unPoseStructSize)
+	}
+
+	fn VsyncEvent(&self, vsyncTimeOffsetSeconds: f64) {
+		self.real.VsyncEvent(vsyncTimeOffsetSeconds)
+	}
+
+	fn VendorSpecificEvent(
+		&self,
+		unWhichDevice: u32,
+		eventType: EVREventType,
+		eventData: *const VREvent_Data_t,
+		eventTimeOffset: f64,
+	) {
+		self.real
+			.VendorSpecificEvent(unWhichDevice, eventType, eventData, eventTimeOffset)
+	}
+
+	fn IsExiting(&self) -> bool {
+		self.real.IsExiting()
+	}
+
+	fn PollNextEvent(&self, pEvent: *mut VREvent_t, uncbVREvent: u32) -> bool {
+		self.real.PollNextEvent(pEvent, uncbVREvent)
+	}
+
+	fn GetRawTrackedDevicePoses(
+		&self,
+		fPredictedSecondsFromNow: f32,
+		pTrackedDevicePoseArray: *mut TrackedDevicePose_t,
+		unTrackedDevicePoseArrayCount: u32,
+	) {
+		self.real.GetRawTrackedDevicePoses(
+			fPredictedSecondsFromNow,
+			pTrackedDevicePoseArray,
+			unTrackedDevicePoseArrayCount,
+		)
+	}
+
+	fn RequestRestart(
+		&self,
+		pchLocalizedReason: *const c_char,
+		pchExecutableToStart: *const c_char,
+		pchArguments: *const c_char,
+		pchWorkingDirectory: *const c_char,
+	) {
+		self.real.RequestRestart(
+			pchLocalizedReason,
+			pchExecutableToStart,
+			pchArguments,
+			pchWorkingDirectory,
+		)
+	}
+
+	fn GetFrameTimings(&self, pTiming: *mut Compositor_FrameTiming, nFrames: u32) -> u32 {
+		self.real.GetFrameTimings(pTiming, nFrames)
+	}
+
+	fn SetDisplayEyeToHead(
+		&self,
+		unWhichDevice: u32,
+		eyeToHeadLeft: *const HmdMatrix34_t,
+		eyeToHeadRight: *const HmdMatrix34_t,
+	) {
+		self.real
+			.SetDisplayEyeToHead(unWhichDevice, eyeToHeadLeft, eyeToHeadRight)
+	}
+
+	fn SetDisplayProjectionRaw(
+		&self,
+		unWhichDevice: u32,
+		eyeLeft: *const HmdRect2_t,
+		eyeRight: *const HmdRect2_t,
+	) {
+		self.real
+			.SetDisplayProjectionRaw(unWhichDevice, eyeLeft, eyeRight)
+	}
+
+	fn SetRecommendedRenderTargetSize(&self, unWhichDevice: u32, nWidth: u32, nHeight: u32) {
+		self.real
+			.SetRecommendedRenderTargetSize(unWhichDevice, nWidth, nHeight)
+	}
+}
+
+pub static DRIVER_HOST: Lazy<WithVtables<DriverHost>> = Lazy::new(|| {
+	let context = DRIVER_CONTEXT
+		.get()
+		.expect("driver context should be initialized at this point");
+	let real = unsafe {
+		&*(context
+			.get_generic_interface(IVRServerDriverHost_Version)
+			.expect("missing server driver host") as *const _
+			as *const VtableRef<IVRServerDriverHostVtable>)
+	};
+	WithVtables::new(DriverHost { real })
+});
---
+++ hmd.rs
@@ -0,0 +1,264 @@
+use std::{
+	ffi::{c_void, CStr},
+	os::raw::c_char,
+	rc::Rc,
+};
+
+use crate::{
+	driver_context::{self, DRIVER_CONTEXT},
+	settings::{set_properties, Property, PropertyValue, PROPERTIES},
+	Result,
+};
+use cppvtbl::{impl_vtables, HasVtable, VtableRef, WithVtables};
+use lens_protocol::{Eye, LensClient};
+use openvr::{
+	k_unFloatPropertyTag, EPropertyWriteType, ETrackedDeviceProperty, ETrackedPropertyError,
+	HmdVector2_t, IVRProperties, PropertyWrite_t,
+};
+use tracing::{error, info, instrument};
+use vive_hid::{Mode, ViveConfig, ViveCosmosDevice};
+
+use crate::openvr::{
+	DistortionCoordinates_t, DriverPose_t, EVREye, EVRInitError, ITrackedDeviceServerDriver,
+	ITrackedDeviceServerDriverVtable, IVRDisplayComponent, IVRDisplayComponentVtable,
+	IVRDisplayComponent_Version,
+};
+
+fn map_eye(eye: EVREye) -> Eye {
+	match eye {
+		EVREye::Eye_Left => Eye::Left,
+		EVREye::Eye_Right => Eye::Right,
+	}
+}
+
+#[impl_vtables(IVRDisplayComponent)]
+struct HmdDisplay {
+	// steam: Rc<SteamDevice>,
+	// vive: Rc<ViveDevice>,
+	lens: Rc<dyn LensClient>,
+	real: &'static VtableRef<IVRDisplayComponentVtable>,
+	mode: Mode,
+}
+
+impl IVRDisplayComponent for HmdDisplay {
+	#[instrument(skip(self))]
+	fn GetWindowBounds(&self, pnX: *mut i32, pnY: *mut i32, pnWidth: *mut u32, pnHeight: *mut u32) {
+		let Mode { width, height, .. } = self.mode;
+		unsafe {
+			*pnX = 0;
+			*pnY = 0;
+			*pnWidth = width;
+			*pnHeight = height;
+		}
+	}
+
+	fn IsDisplayOnDesktop(&self) -> bool {
+		self.real.IsDisplayOnDesktop()
+	}
+
+	fn IsDisplayRealDisplay(&self) -> bool {
+		self.real.IsDisplayRealDisplay()
+	}
+
+	fn GetRecommendedRenderTargetSize(&self, pnWidth: *mut u32, pnHeight: *mut u32) {
+		let Mode { width, height, .. } = self.mode;
+		unsafe {
+			*pnWidth = width / 2;
+			*pnHeight = height;
+		}
+	}
+
+	#[instrument(skip(self))]
+	fn GetEyeOutputViewport(
+		&self,
+		eEye: EVREye,
+		pnX: *mut u32,
+		pnY: *mut u32,
+		pnWidth: *mut u32,
+		pnHeight: *mut u32,
+	) {
+		// let err: Result<()> = try {
+		let Mode { width, height, .. } = self.mode;
+		unsafe {
+			*pnX = if eEye == EVREye::Eye_Left {
+				0
+			} else {
+				width / 2
+			};
+			*pnY = 0;
+			*pnWidth = width / 2;
+			*pnHeight = height;
+		}
+		// 	return;
+		// };
+		// error!("failed: {}", err.err().unwrap());
+		// self.real
+		// 	.GetEyeOutputViewport(eEye, pnX, pnY, pnWidth, pnHeight)
+	}
+
+	#[instrument(skip(self))]
+	fn GetProjectionRaw(
+		&self,
+		eEye: EVREye,
+		pfLeft: *mut f32,
+		pfRight: *mut f32,
+		pfTop: *mut f32,
+		pfBottom: *mut f32,
+	) {
+		let err: Result<()> = try {
+			let result = self.lens.project(map_eye(eEye))?;
+			unsafe {
+				*pfLeft = result.left;
+				*pfRight = result.right;
+				if self.lens.matrix_needs_inversion()? {
+					*pfTop = result.bottom;
+					*pfBottom = result.top;
+				} else {
+					*pfTop = result.top;
+					*pfBottom = result.bottom;
+				}
+			}
+			return;
+		};
+		error!("failed: {}", err.err().unwrap());
+		self.real
+			.GetProjectionRaw(eEye, pfLeft, pfRight, pfTop, pfBottom)
+	}
+
+	#[instrument(skip(self))]
+	fn ComputeDistortion(&self, eEye: EVREye, fU: f32, fV: f32) -> DistortionCoordinates_t {
+		let err: Result<()> = try {
+			let inverse = self.lens.matrix_needs_inversion()?;
+			let result = self
+				.lens
+				.distort(map_eye(eEye), [fU, if inverse { 1.0 - fV } else { fV }])?;
+			return DistortionCoordinates_t {
+				rfRed: result.red,
+				rfGreen: result.green,
+				rfBlue: result.blue,
+			};
+		};
+		error!("failed: {}", err.err().unwrap());
+		self.real.ComputeDistortion(eEye, fU, fV)
+	}
+
+	fn ComputeInverseDistortion(
+		&self,
+		_idk1: *mut HmdVector2_t,
+		_eEye: EVREye,
+		_fU: f32,
+		_fV: f32,
+	) -> i32 {
+		// Not entirely sure what should this function do, but original impl has only `xor eax eax; retn` inside,
+		// so fine by me. Not delegating to real display, to prevent it from somehow breaking in the future.
+		0
+	}
+}
+
+#[impl_vtables(ITrackedDeviceServerDriver)]
+pub struct HmdDriver {
+	pub vive: Rc<ViveCosmosDevice>,
+	pub vive_config: ViveConfig,
+	pub lens: Rc<dyn LensClient>,
+	pub real: &'static VtableRef<ITrackedDeviceServerDriverVtable>,
+	pub mode: Mode,
+}
+
+impl ITrackedDeviceServerDriver for HmdDriver {
+	fn Activate(&self, unObjectId: u32) -> EVRInitError {
+		let res = self.real.Activate(unObjectId);
+		if res != EVRInitError::VRInitError_None {
+			return res;
+		}
+		let container = PROPERTIES.TrackedDeviceToPropertyContainer(unObjectId);
+
+		set_properties(
+			container,
+			vec![
+				Property::new(
+					ETrackedDeviceProperty::Prop_DisplayFrequency_Float,
+					PropertyValue::Float(self.mode.frame_rate),
+				),
+				Property::new(
+					ETrackedDeviceProperty::Prop_DisplaySupportsMultipleFramerates_Bool,
+					PropertyValue::Bool(true),
+				),
+				Property::new(
+					ETrackedDeviceProperty::Prop_SecondsFromVsyncToPhotons_Float,
+					PropertyValue::Float(
+						(1.0 / self.mode.frame_rate) + self.mode.extra_photon_vsync,
+					),
+				),
+				// Property::new(
+				// 	ETrackedDeviceProperty::Prop_MinimumIpdStepMeters_Float,
+				// 	PropertyValue::Float(0.0005),
+				// ),
+				// Property::new(
+				// 	ETrackedDeviceProperty::Prop_UserIpdMeters_Float,
+				// 	// TODO
+				// 	PropertyValue::Float(0.0005),
+				// ),
+				Property::new(
+					ETrackedDeviceProperty::Prop_UserHeadToEyeDepthMeters_Float,
+					PropertyValue::Float(0.015),
+				),
+				Property::new(
+					ETrackedDeviceProperty::Prop_DisplayAvailableFrameRates_Float_Array,
+					PropertyValue::FloatArray(if self.mode.frame_rate == 90.0 {
+						vec![90.0, 120.0]
+					} else {
+						vec![120.0, 90.0]
+					}),
+				),
+				Property::new(
+					ETrackedDeviceProperty::Prop_DisplaySupportsRuntimeFramerateChange_Bool,
+					PropertyValue::Bool(true),
+				),
+			],
+		);
+
+		EVRInitError::VRInitError_None
+	}
+
+	fn Deactivate(&self) {
+		self.real.Deactivate()
+	}
+
+	fn EnterStandby(&self) {
+		self.real.EnterStandby()
+	}
+
+	fn GetComponent(&self, pchComponentNameAndVersion: *const c_char) -> *mut c_void {
+		let name = unsafe { CStr::from_ptr(pchComponentNameAndVersion) };
+		info!("getting {name:?} hmd component");
+		let real = self.real.GetComponent(pchComponentNameAndVersion);
+		if name == unsafe { CStr::from_ptr(IVRDisplayComponent_Version) } {
+			info!("faking display");
+			let display = Box::leak(Box::new(WithVtables::new(HmdDisplay {
+				// steam: self.steam.clone(),
+				// vive: self.vive.clone(),
+				lens: self.lens.clone(),
+				real: unsafe { VtableRef::from_raw(real as *const _) },
+				mode: self.mode,
+			})));
+			VtableRef::into_raw_mut(HasVtable::<IVRDisplayComponentVtable>::get_mut(display))
+				as *mut _
+		} else {
+			real
+		}
+	}
+
+	fn DebugRequest(
+		&self,
+		pchRequest: *const c_char,
+		pchResponseBuffer: *mut c_char,
+		unResponseBufferSize: u32,
+	) {
+		self.real
+			.DebugRequest(pchRequest, pchResponseBuffer, unResponseBufferSize)
+	}
+
+	fn GetPose(&self) -> DriverPose_t {
+		self.real.GetPose()
+	}
+}
